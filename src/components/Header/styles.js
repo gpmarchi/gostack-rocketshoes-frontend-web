ab2bdf041500be7e2125fe0abad1a0d998a9b547
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-02-29 19:46:26 -0300

    style cart component with opacity

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -12,6 +12,11 @@ export const Cart = styled(Link)`
   display: flex;
   align-items: center;
   text-decoration: none;
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 0.7;
+  }
 
   div {
     text-align: right;
